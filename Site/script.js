fedee97a36cf1d88100a8ee2028117068a86a983
Francisco Sousa <f.sou@hotmail.com>
2018-03-12 00:57:46 +0000

    Botões e tabs e cores (#2)
    * site - identation
    
    * site - tabs
    
    * site - buttons style
    
    * site - buttons style
    
    * site - closable tabs + colors

---
+++ Site/script.js
@@ -0,0 +1,25 @@
+function changeTab(button, section) {
+    var i, sections, buttonEl;
+
+    buttonEl = document.getElementById(button);
+
+    sections = document.getElementsByClassName("section");
+
+    for (i = 0; i < sections.length; i++) {
+        sections[i].style.display = "none";
+    }
+    
+    if (!buttonEl.classList.contains("active")) {
+
+        document.getElementById(section).style.display = "block";
+
+        buttons = document.getElementsByClassName("tab");
+        for (i = 0; i < buttons.length; i++) {
+            buttons[i].className = buttons[i].className.replace(" active", "")
+        }
+
+        buttonEl.className += " active";
+    } else {
+        buttonEl.className = buttonEl.className.replace(" active", "");
+    }
+}
