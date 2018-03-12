8cc42bc1a25765a636b7c5fb003c3112e6bde50e
Francisco Sousa <f.sou@hotmail.com>
2018-03-12 16:01:53 +0000

    links exteriores agora em ficheiro separado

---
+++ Site/links.js
@@ -0,0 +1,82 @@
+var links = [
+	{
+		"id": "q1",
+		"url": "questionario1.html"
+	},
+	{
+		"id": "q2",
+		"url": "questionario2.html"
+	},
+	{
+		"id": "at1",
+		"url": "at1.html"
+	},
+	{
+		"id": "at2",
+		"url": "at2.html"
+	},
+	{
+		"id": "modc1",
+		"url": "mod-conceptual1.html"
+	},
+	{
+		"id": "modc2",
+		"url": "mod-conceptual2.html"
+	},
+	{
+		"id": "pbf1",
+		"url": "pbf1.html"
+	},
+	{
+		"id": "pbf2",
+		"url": "pbf2.html"
+	},
+	{
+		"id": "pf1b",
+		"url": "protFuncional1-beta.html"
+	},
+	{
+		"id": "pf1c",
+		"url": "protFuncional1-completo.html"
+	},
+	{
+		"id": "pf2b",
+		"url": "protFuncional2-beta.html"
+	},
+	{
+		"id": "pf2c",
+		"url": "protFuncional2-completo.html"
+	},
+	{
+		"id": "pfb",
+		"url": "protFinal-beta.html.html"
+	},
+	{
+		"id": "pfc",
+		"url": "protFinal-completo.html"
+	},
+	{
+		"id": "tc1",
+		"url": "tarefas-criterios1.html"
+	},
+	{
+		"id": "tc2",
+		"url": "tarefas-criterios2.html"
+	},
+	{
+		"id": "rt1",
+		"url": "relatorioTestes1.html"
+	},
+	{
+		"id": "rt2",
+		"url": "relatorioTestes2.html"
+	},
+	{
+		"id": "man",
+		"url": "manual.html"
+	}
+]
+
+function getData() {
+	return links;
+}
