3932b4035afbfbb3cb488095736bc4dc022b5519
Francisco Sousa <f.sou@hotmail.com>
2018-04-10 00:05:36 +0100

    Agora é flex! Notificações dão scroll fixe

--- Relogio/script.js
+++ Relogio/script.js
@@ -1,22 +1,42 @@
-function showClock(clock) {
+var notifications = [new Notification("Ola", "../Site/assets/student3.png")];
+
+function updateClock(clock) {
     var time = new Date();
     document.getElementById(clock).innerHTML = "";
     if (time.getHours() < 10) {
         document.getElementById(clock).innerHTML += "0";
-    } 
+    }
     document.getElementById(clock).innerHTML += time.getHours() + ":";
     if (time.getMinutes() < 10) {
         document.getElementById(clock).innerHTML += "0";
     }
     document.getElementById(clock).innerHTML += time.getMinutes();
-    setTimeout(function() {
-        showClock(clock);
+    setTimeout(function () {
+        updateClock(clock);
     }, 1000);
 }
 
 function appendToList() {
     var node = document.createElement("DIV");
-    var textnode = document.createTextNode("Notification");
-    node.appendChild(textnode);
-    document.getElementById("notifaction-bar").appendChild(node);
+    node.innerHTML =
+    '<table class="notification">\
+        <tr>\
+            <td class="notif-icon">\
+                <img src="" width="6pt">\
+			</td>\
+            <td class="notif-text">o</td>\
+		</tr>\
+    </table>';
+    table = node.firstElementChild.firstElementChild.getElementsByTagName("TD");
+    table[0].firstElementChild.setAttribute("src", notifications[0]["img"]);
+    table[1].innerText = notifications[0]["name"];
+    document.getElementById("notification-bar").appendChild(node);
+}
+
+function Notification(name, img) {
+    this.name = name;
+    this.img = img;
+    /*this.changeName = function (name) {
+        this.lastName = name;
+    };*/
 }
