233b413ac7bf7cc920b66f1d4e5f4820e4eff8b5
Daniel Fernandes <dani.s-fernandes@hotmail.com>
2018-04-09 18:14:51 +0100

    bugs do clock fixed

--- Relogio/script.js
+++ Relogio/script.js
@@ -1,5 +1,13 @@
 function showClock() {
     var time = new Date();
-    document.getElementById("clock").innerHTML = time.getHours() + ":" + time.getMinutes();
+    document.getElementById("clock").innerHTML = "";
+    if (time.getHours() < 10) {
+        document.getElementById("clock").innerHTML += "0";
+    } 
+    document.getElementById("clock").innerHTML += time.getHours() + ":";
+    if (time.getMinutes < 10) {
+        document.getElementById("clock").innerHTML = "0";
+    }
+    document.getElementById("clock").innerHTML += time.getMinutes();
     setTimeout(showClock, 1000);
 }
