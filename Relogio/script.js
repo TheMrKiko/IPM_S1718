e12db321ac8553ee3dd82af25aa83bb6c3d2c2c3
Daniel Fernandes <dani.s-fernandes@hotmail.com>
2018-04-09 19:05:30 +0100

    Ecrãs e horas no principal

--- Relogio/script.js
+++ Relogio/script.js
@@ -1,15 +1,17 @@
-function showClock() {
+function showClock(clock) {
     var time = new Date();
-    document.getElementById("clock").innerHTML = "";
+    document.getElementById(clock).innerHTML = "";
     if (time.getHours() < 10) {
-        document.getElementById("clock").innerHTML += "0";
-    }
-    document.getElementById("clock").innerHTML += time.getHours() + ":";
+        document.getElementById(clock).innerHTML += "0";
+    } 
+    document.getElementById(clock).innerHTML += time.getHours() + ":";
     if (time.getMinutes < 10) {
-        document.getElementById("clock").innerHTML = "0";
+        document.getElementById(clock).innerHTML = "0";
     }
-    document.getElementById("clock").innerHTML += time.getMinutes();
-    setTimeout(showClock, 1000);
+    document.getElementById(clock).innerHTML += time.getMinutes();
+    setTimeout(function() {
+        showClock(clock);
+    }, 1000);
 }
 
 function appendToList() {
