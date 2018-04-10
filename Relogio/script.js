919ccd8a3d73fbab660466adcb4b55612ec49804
Francisco Sousa <f.sou@hotmail.com>
2018-04-10 23:39:17 +0100

    Relógio grande com notificações modeladas, em flex (#40)
    * Adicionar Notificações
    
    * Ecrãs e horas no principal
    
    * clock bug fixed
    
    * pt -> en
    
    * Agora é flex! Notificações dão scroll fixe
    
    * Botão removido

--- Relogio/script.js
+++ Relogio/script.js
@@ -1,13 +1,42 @@
-function showClock() {
+var notifications = [new Notification("Ola", "../Site/assets/student3.png")];
+
+function updateClock(clock) {
     var time = new Date();
-    document.getElementById("clock").innerHTML = "";
+    document.getElementById(clock).innerHTML = "";
     if (time.getHours() < 10) {
-        document.getElementById("clock").innerHTML += "0";
-    } 
-    document.getElementById("clock").innerHTML += time.getHours() + ":";
-    if (time.getMinutes < 10) {
-        document.getElementById("clock").innerHTML = "0";
+        document.getElementById(clock).innerHTML += "0";
     }
-    document.getElementById("clock").innerHTML += time.getMinutes();
-    setTimeout(showClock, 1000);
+    document.getElementById(clock).innerHTML += time.getHours() + ":";
+    if (time.getMinutes() < 10) {
+        document.getElementById(clock).innerHTML += "0";
+    }
+    document.getElementById(clock).innerHTML += time.getMinutes();
+    setTimeout(function () {
+        updateClock(clock);
+    }, 1000);
+}
+
+function appendToList() {
+    var node = document.createElement("DIV");
+    node.innerHTML =
+    '<table class="notification">\
+        <tr>\
+            <td class="notif-icon">\
+                <img src="" width="6pt">\
+			</td>\
+            <td class="notif-text">o</td>\
+		</tr>\
+    </table>';
+    table = node.firstElementChild.firstElementChild.getElementsByTagName("TD");
+    table[0].firstElementChild.setAttribute("src", notifications[0]["img"]);
+    table[1].innerText = notifications[0]["name"];
+    document.getElementById("notification-bar").appendChild(node);
+}
+
+function Notification(name, img) {
+    this.name = name;
+    this.img = img;
+    /*this.changeName = function (name) {
+        this.lastName = name;
+    };*/
 }
