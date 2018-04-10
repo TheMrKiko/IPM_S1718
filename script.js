d53ca2df7be6a0b6253a6e975c7fa065e6e67a55
João Daniel Silva <joao.daniel.silva@tecnico.ulisboa.pt>
2018-04-10 21:55:34 +0100

    Joystick que lê o rato quando se o arrasta
    sobre o #39

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
- 
+var stickerPos; //global variable
+
+//ondragstart
+function readStickerPos(event) {
+    stickerPos = event.clientX;
+    document.getElementById("status").innerHTML = stickerPos;
+}
+
+
+//event listener to allow the record of mouse position while dragging
+document.addEventListener("dragover", function(e){
+    e = e || window.event;
+    posCursorX = e.clientX;
+    //console.log("X: "+posCursorX);
+}, false);
+
+
+//ondragend
+function registerPosition(event) {
+    var posX;
+    posX = event.clientX;
+    document.getElementById("status").innerHTML = "fim: " + posX;
+}
