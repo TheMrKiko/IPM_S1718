5b8bbe97071ea45591fd7e55a66218d0c68762df
Daniel Fernandes <dani.s-fernandes@hotmail.com>
2018-04-13 16:55:47 +0100

    novo styling de botões

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
- 
+function buttonPress(button) {
+    
+
+} 
+
+function scrollTo(element){
+    var topPos = document.getElementById(element).offsetTop;
+    document.getElementById('container').scrollTop += 10;
+}
