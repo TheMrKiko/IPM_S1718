2ee2226d4df6f18f58f5574e5cd7bcb60607e60e
Daniel Fernandes <dani.s-fernandes@hotmail.com>
2018-04-17 20:09:30 +0100

    Aplicação dos Amigos #45 (#54)
    * Iniciado screen
    
    * na app amigos já há carinhas
    
    * screen amigos dá horas, Rogério passou a David, Daniel não gira :(
    
    * distância em cada amigo, algoritmo sort, ainda não substitui
    
    * give up do sort, distância aleatória e ordenada a cada amigo com posição fixa na grelha
    
    * typo fixed
    
    * screen mapa e bússola; inciando o rodar da bússola
    
    * Imagens para a pasta assets
    
    * Obj Pessoa
    
    * Imagens de pessoas
    
    * Mapa com um lemur
    
    * Hidden models from watch/main-screen
    
    * Objectos Pessoa são adicionados dinamicamente :O
    
    * friend detail screen
    
    * Header com transparencia do watch/screens
    
    * header fixo
    
    * Header com título e botão
    
    * anda de um lado para o outro sort of
    
    * Footer nas funcionalidades (#49)
    
    * footer com botão Fim no ecrã amigos
    
    * footer added com FLEX
    
    * Hidden models, para fazer um para o footer (#48)
    
    * Adicionar Notificações
    
    * Ecrãs e horas no principal
    
    * clock bug fixed
    
    * pt -> en
    
    * Agora é flex! Notificações dão scroll fixe
    
    * Botão removido
    
    * Foto de notificação maior e texto laranja
    
    * Hidden models no html
    
    * Função para dar atributos aos hiddens models
    
    * Flex estranho
    
    * Footer com tamanho adequado
    
    * Fix do merge
    
    * Início do modelo de headers e footers
    
    * tentativa de mapa

--- Relogio/script.js
+++ Relogio/script.js
@@ -1,13 +1,15 @@
 var notifications = [new Notification("à sua procura.", "../assets/joao.jpg"), new Notification("gosta de si.", "../assets/joao.jpg"), new Notification("tem saudades", "../assets/joao.jpg")];
+var notifN = 0;
+var i = 0;
+var people = [new Person("Daniel", "assets/people/joe-gardner.jpg"), new Person("João", "assets/people/erik-lucatero.jpg"), new Person("Francisco", "assets/people/bill-jones-jr.jpg"), new Person("David", "assets/people/parker-whitson.jpg"), new Person("Luís", "assets/people/sam-burriss.jpg"), new Person("Rodrigo", "assets/people/hunter-johnson.jpg"), new Person("Maria", "assets/people/noah-buscher.jpg"), new Person("Marta", "assets/people/hian-oliveira.jpg")];
 var screens = [new Screen("Lock", "lockScreen", "", "lockScreen", ""),
                new Screen("Main", "mainScreen", "appendToList()", "mainSolo", "lockScreen"), 
                new Screen("App", "appScreen", "console.log('app')", "mainSolo", "mainScreen"),
-               new Screen("Friends", "friendScreen", "", "friendScreen", "appScreen")
+               new Screen("Friends", "friendScreen", "showPeople();", "friendScreen", "appScreen")
             ];
 var currentSolo;
 var currentScreen = "mainScreen";
 /*var currentSwipe;*/
-var notifN = 0;
 
 function loadClocks() {
     var clocks = document.getElementsByClassName("clock");
@@ -34,7 +36,7 @@ function updateClock(clock) {
 }
 
 function cloneElement(classModel) {
-    var model = document.getElementById("models").getElementsByClassName(classModel)[0];
+    var model = document.getElementById("models").getElementsByClassName(classModel)[0].firstElementChild;
     return model.cloneNode(true);
 }
 
@@ -59,12 +61,46 @@ function appendToList() {
     cloneElementTo("table-model", "notification-bar", notifications[cur]["img"], notifications[cur]["name"]);
 }
 
+function showPeople() {
+	for (var i = 0; i < people.length; i++) {
+        cloneElementTo("person-model", "gridFriends", people[i]["img"], people[i]["name"], people[i]["distance"]);
+    }
+}
+
 function Notification(name, img) {
     this.name = name;
     this.img = img;
     /*this.changeName = function (name) {
         this.lastName = name;
     };*/
+}
+
+function Person(name, img) {
+    this.name = name;
+    this.img = img;
+	this.distance = rD();
+    /*this.changeName = function (name) {
+        this.lastName = name;
+    };*/
+}
+
+function randomNumberGenerator(myMin, myMax) {
+  return Math.floor(Math.random()*(myMax - myMin + 1) + myMin);
+}
+
+function randomDistance() {
+  var distances = document.getElementsByClassName("distance");
+  for (var d=0; d<distances.length; d++) {
+    distances[d].innerHTML = randomNumberGenerator(d * 150, (d+1) * 150);
+  }
+
+}
+
+function rD() {
+	console.log("oi " + i);
+	var distance = randomNumberGenerator(i * 50, (i+1) * 50);
+	i = i + 1;
+	return distance;
 }
 
 function Screen(name, id, initf, solo, homeButton /*, header, footer*/) {
@@ -209,5 +245,4 @@ function swipe(elem, pos, target, dir, offset) {
             elem.style.transform = "translateX(" + rpos + "px)"; 
         }
     }, 5);
-    
 }
