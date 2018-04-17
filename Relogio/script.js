8fea620faf12ba52188217ef5f69a48336c08469
Francisco Sousa <f.sou@hotmail.com>
2018-04-17 14:12:09 +0100

    Loads Solos e Screens

--- Relogio/script.js
+++ Relogio/script.js
@@ -1,7 +1,8 @@
 var notifications = [new Notification("Ola", "../Site/assets/student3.png")];
-//var screens = [new Screen("Main", "main-screen", )]
-var currentScreen;
-var currentSwipeScreen = "mainScreen";
+var screens = [new Screen("Main", "mainScreen", "console.log('main');", "mainSolo"), new Screen("App", "appScreen", "console.log('app')", "mainSolo")];
+var currentSolo;
+var currentScreenInSwipe = "mainScreen";
+var currentSwipe;
 
 function updateClock(clock) {
     var time = new Date();
@@ -45,24 +46,43 @@ function Notification(name, img) {
     };*/
 }
 
-function Screen(name, id, initf, header, footer) {
+function Screen(name, id, initf, solo/*, header, footer*/) {
     this.name = name;
     this.id = id;
     this.inift = initf;
-    this.header = header;
-    this.footer = footer;
-    this.loadScreen = function (name) {
+    this.solo = solo;
+    /*this.header = header;*/
+    /*this.footer = footer;*/
+    this.loadScreen = function () {
         eval(initf);
     };
 }
 
-function soloScreen(screenID) {
-    currentScreen = screenID;
-    screens = document.getElementsByClassName("solo");
-    for (var s = 0; s < screens.length; s++) {
-        screens[s].style.display = "none";
+//loadscreen loada solos
+//loadsolo recebe argumento do screen se houver swipe e faz move
+
+function loadScreen(screenID) {
+    function findScreenWithID(screen) {
+        return screen["id"] == screenID;
     }
-    document.getElementById(screenID).style.display = "flex";
+    var screenObj = screens.find(findScreenWithID);
+    screenObj.loadScreen();
+    loadSolo(screenObj["solo"]);    
+}
+
+function loadSolo(soloID) {
+    //se swipe igual, anima; se diferente, move logo
+    currentSolo = soloID;
+    currentSwipe = soloID;
+    showSolo(soloID);
+}
+
+function showSolo(soloID) {
+    solos = document.getElementsByClassName("solo");
+    for (var s = 0; s < solos.length; s++) {
+        solos[s].style.display = "none";
+    }
+    document.getElementById(soloID).style.display = "flex";
 }
 
 window.addEventListener('message', function (event) {
@@ -76,7 +96,7 @@ window.addEventListener('message', function (event) {
             break;
 
         case "home":
-            soloScreen("mainScreen");
+            loadScreen("mainScreen");
             break;
 
         default:
@@ -86,7 +106,7 @@ window.addEventListener('message', function (event) {
 });
 
 function scrollCurrScreen(value) {
-    scrollElement = document.getElementById(currentScreen);
+    scrollElement = document.getElementById(currentSolo);
     scrollValue(scrollElement, value);
 }
 
@@ -108,39 +128,41 @@ function dragging(ev) {
     /*var then = ev.dataTransfer.getData("Text");*/
     var now = ev.clientX;
     var diff = now - then;
-    if (currentSwipeScreen == "mainScreen") {
+    if (currentScreenInSwipe == "mainScreen") {
         if (diff < 0) {
             var string = "translateX(" + diff + "px)";
-            document.getElementById("mainS").style.transform = string;
+            document.getElementById("mainSolo").style.transform = string;
         }
     }
-    if (currentSwipeScreen == "appScreen") {
+    if (currentScreenInSwipe == "appScreen") {
         if (diff > 0) {
-            diff -= document.getElementById("appScreen").clientWidth;
+            diff -= document.getElementById(currentSwipe).clientWidth;
             var string = "translateX(" + diff + "px)";
-            document.getElementById("mainS").style.transform = string;
+            document.getElementById("mainSolo").style.transform = string;
         }
     }
+
 }
+
 
 function drop(ev) {
     ev.preventDefault();
     var width = document.getElementById("appScreen").clientWidth;
     var now = ev.clientX;
     var then = ev.dataTransfer.getData("Text");
-    if (currentSwipeScreen == "mainScreen") {
+    if (currentScreenInSwipe == "mainScreen") {
         if (then - now >= 50) {
-            swipe(document.getElementById("mainS"), now - then, -width, -1, 0);
-            currentSwipeScreen = "appScreen";
+            swipe(document.getElementById("mainSolo"), now - then, -width, -1, 0);
+            currentScreenInSwipe = "appScreen";
         } else if (then - now > 0){
-            swipe(document.getElementById("mainS"), now - then, 0, 1, 0);
+            swipe(document.getElementById("mainSolo"), now - then, 0, 1, 0);
         }
-    } else if (currentSwipeScreen == "appScreen") {
+    } else if (currentScreenInSwipe == "appScreen") {
         if (now - then >= 50) {
-            swipe(document.getElementById("mainS"), now - then, width, 1, -width);
-            currentSwipeScreen = "mainScreen";
+            swipe(document.getElementById("mainSolo"), now - then, width, 1, -width);
+            currentScreenInSwipe = "mainScreen";
         } else if (now - then > 0){
-            swipe(document.getElementById("mainS"), now - then, 0, -1, -width);
+            swipe(document.getElementById("mainSolo"), now - then, 0, -1, -width);
         }
     }
 }
