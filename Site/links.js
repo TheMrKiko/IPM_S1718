7c3c7b402fef3535b9c4d9165ed491cfa2f6fbbb
Francisco Sousa <f.sou@hotmail.com>
2018-05-25 00:09:56 +0100

    Site (#84)
    Links de funcionalidades e guião de análise dos utilizadores

--- Site/links.js
+++ Site/links.js
@@ -55,28 +55,28 @@ var links = [
 	},
 	{
 		"id": "pf2b",
-		"url": "protFuncional2-beta.html",
-		"exists": "false"
+		"url": "http://web.tecnico.ulisboa.pt/ist186416/IPM/pf2b/index.html",
+		"exists": "true"
 	},
 	{
 		"id": "pf2c",
-		"url": "protFuncional2-completo.html",
-		"exists": "false"
+		"url": "http://web.tecnico.ulisboa.pt/ist186416/IPM/pf2c/index.html",
+		"exists": "true"
 	},
 	{
 		"id": "pfb",
-		"url": "protFinal-beta.html.html",
-		"exists": "false"
+		"url": "http://web.tecnico.ulisboa.pt/ist186416/IPM/pfb/index.html",
+		"exists": "true"
 	},
 	{
 		"id": "pfc",
-		"url": "protFinal-completo.html",
-		"exists": "false"
+		"url": "http://web.tecnico.ulisboa.pt/ist186416/IPM/pfc/index.html",
+		"exists": "true"
 	},
 	{
 		"id": "tc1",
-		"url": "tarefas-criterios1.html",
-		"exists": "false"
+		"url": "../Entregas/4 - Avaliacao com os Utilizadores/Tarefas e Criterios de usabilidade.pdf",
+		"exists": "true"
 	},
 	{
 		"id": "tc2",
